01fe45af6ff77e91de4e8f7afb07c52d2414a54c
amirine <zhuk.irine@gmail.com>
2021-07-15 20:37:12 +0300

    Added admin check

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import sys
+import settings
 
 
 def start(args=sys.argv) -> None:
 	"""Prints Hello"""
-	print(f'Hello, {args[-1]}!')
+	if args[1] == settings.admin.get('login') and \
+	   args[2] == settings.admin.get('password'):
+		print(f'You entered!')
+	else:
+		print(f'Wrong input!')
---
+++ settings.py
@@ -0,0 +1,4 @@
+admin = {
+	'login': 'ira',
+	'password': '1111',
+}
