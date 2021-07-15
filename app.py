ba0d94fae9f27f1d85dd86ff184861f0666ebc26
amirine <zhuk.irine@gmail.com>
2021-07-15 20:44:28 +0300

    Added valid check for input arguments

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import settings
 
 
 def start(args=sys.argv) -> None:
-	"""Prints Hello"""
-	if args[1] == settings.admin.get('login') and \
+	"""Checks admin entering data"""
+	if len(args) == 3 and args[1] == settings.admin.get('login') and \
 	   args[2] == settings.admin.get('password'):
 		print(f'You entered!')
 	else:
